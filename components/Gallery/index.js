14306227e44328a225b3d807ac029bd5287662c4
LUIS ANAYA <49276647+laanayam333@users.noreply.github.com>
2020-12-27 02:16:38 +0100

    redesigned the pages heros to fix excessive white space, everythings mostly done, need to fix issue with gallery scroll animation

--- components/Gallery/index.js
+++ components/Gallery/index.js
@@ -30,40 +30,44 @@ const Gallery = ({ gallery, onCursor }) => {
 
   return (
     <>
-      <section className="w-full grid grid-cols-2 lg:grid-cols-4 gap-10 lg:gap-14 2xl:gap-16 tw-separator-t py-10 lg:py-14 2xl:py-16 tw-container-sm bg-gray-100 dark:bg-gray-900">
-        {gallery.fields.map((elm, index) => (
-          <motion.div
-            className="flex flex-col justify-center"
-            key={index}
-            ref={contentRef}
-            initial="hidden"
-            animate={animation}
-            variants={scrollVariants}
-          >
-            <Image
-              key={index}
-              src={elm.photo.url}
-              alt={elm.photo.alt}
-              layout="responsive"
-              objectFit="cover"
-              quality={75}
-              width={elm.photo.dimensions.width}
-              height={elm.photo.dimensions.height}
-              onClick={() => handleModalToggle(index)}
-              onMouseEnter={() => onCursor('tw-hovered')}
-              onMouseLeave={onCursor}
-            />
-          </motion.div>
-        ))}
-      </section>
+      {gallery && (
+        <section>
+          <div className="w-full grid grid-cols-2 lg:grid-cols-4 gap-10 lg:gap-14 2xl:gap-16 tw-separator-t py-10 lg:py-14 2xl:py-16 tw-container-sm bg-gray-100 dark:bg-gray-900">
+            {gallery.fields.map((elm, index) => (
+              <motion.div
+                className="flex flex-col justify-center"
+                key={index}
+                ref={contentRef}
+                initial="hidden"
+                animate={animation}
+                variants={scrollVariants}
+              >
+                <Image
+                  key={index}
+                  src={elm.photo.url}
+                  alt={elm.photo.alt}
+                  layout="responsive"
+                  objectFit="cover"
+                  quality={75}
+                  width={elm.photo.dimensions.width}
+                  height={elm.photo.dimensions.height}
+                  onClick={() => handleModalToggle(index)}
+                  onMouseEnter={() => onCursor('tw-hovered')}
+                  onMouseLeave={onCursor}
+                />
+              </motion.div>
+            ))}
+          </div>
 
-      <Modal
-        gallery={gallery}
-        selected={selected}
-        isModalOpen={isModalOpen}
-        handleModalToggle={handleModalToggle}
-        onCursor={onCursor}
-      />
+          <Modal
+            gallery={gallery}
+            selected={selected}
+            isModalOpen={isModalOpen}
+            handleModalToggle={handleModalToggle}
+            onCursor={onCursor}
+          />
+        </section>
+      )}
     </>
   );
 };
